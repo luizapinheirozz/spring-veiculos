971a6b501359a6c3a7ab5ae6a1fc87ce40b6b402
Luiza Pinheiro <luizapinheiro.dev@gmail.com>
2024-05-11 04:35:22 -0300

    Configuração inicial Spring

---
+++ Veiculos-Santana/src/main/java/com/veic/Veiculos/Santana/VeiculosSantanaApplication.java
@@ -0,0 +1,13 @@
+package com.veic.Veiculos.Santana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosSantanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosSantanaApplication.class, args);
+	}
+
+}
---
+++ Veiculos-Santana/src/test/java/com/veic/Veiculos/Santana/VeiculosSantanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veic.Veiculos.Santana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosSantanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
